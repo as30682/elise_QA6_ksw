0892b4ce61900977216719a7eeea8b141fe39079
as30682 <qwas30682@gmail.com>
2026-08-06 11:17:42 +0900

    A 브랜치에서 내용 수정

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-# 100에서 1까지 숫자를 출력. 단, 369 게임처럼 3, 6, 9 을 포함하고 있는 경우, 갯수만큼 “짝” 을 출력. (33일경우 “짝짝”)
-
-
-for n in range(100, 0, -1):
-    count = 0
-    for c in str(n):
-        if c in "369":
-            count += 1
-    if count > 0:
-        print("짝" * count)
-    else:
-        print(n)
+print("A 브랜치 내용")
